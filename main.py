1cd8cc21d1ec5567d0df31de5fe00f1470369b89
zeus7dc <91213611+zeus7dc@users.noreply.github.com>
2024-06-12 20:17:01 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+# IDs dos usuários do Discord cujos avatares queremos buscar
+user_ids = ['677705088822804506', '1044371077041770577', '1145041600980992030']
+
+# Função para buscar o avatar de um usuário específico
+def fetch_avatar(user_id):
+    try:
+        url = f"https://pfpfinder.com/api/discord/user/{user_id}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json().get('avatar')  # Assume que a API retorna o link direto para o avatar
+        else:
+            print(f"Erro ao buscar avatar para o usuário {user_id}. Status Code: {response.status_code}")
+            return None
+    except Exception as e:
+        print(f"Erro ao buscar avatar para o usuário {user_id}: {str(e)}")
+        return None
+
+# Rota para cada usuário
+@app.route('/<user_id>')
+def avatar_page(user_id):
+    avatar_url = fetch_avatar(user_id)
+    if avatar_url:
+        return render_template('avatar.html', avatar_url=avatar_url)
+    else:
+        return 'Avatar não encontrado'
+
+if __name__ == '__main__':
+    app.run(debug=True)
